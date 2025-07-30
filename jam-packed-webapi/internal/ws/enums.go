258297a04d84e0f968d980d7aeb68548d470c4c8
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-30 23:35:22 +1000

    message event enum

---
+++ jam-packed-webapi/internal/ws/enums.go
@@ -0,0 +1,11 @@
+package ws
+
+const (
+	EventUserJoined   = "user_joined"
+	EventUserLeft     = "user_left"
+	EventError        = "error"
+	EventPromptPin    = "prompt_pin"
+	EventIncorrectPin = "incorrect_pin"
+	EventCurrentUsers = "current_users"
+	// Job status enum values...
+)
