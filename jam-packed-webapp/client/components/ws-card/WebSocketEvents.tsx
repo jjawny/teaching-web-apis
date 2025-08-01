16d8ffe4a5f5316e759857aff82687bdee25d33c
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-01 14:07:36 +1000

    extract overlays

--- jam-packed-webapp/client/components/ws-card/WebSocketEvents.tsx
+++ jam-packed-webapp/client/components/ws-card/WebSocketEvents.tsx
@@ -17,34 +17,13 @@ export default function WebSocketEvents({
       <div
         onClick={() => setEventLogExpanded((v) => !v)}
         className={cn(
-          "relative",
+          "relative", // for overlays
           "border border-blue-100",
           "cursor-pointer overflow-hidden rounded-[2.5rem] select-none",
           "bg-gradient-to-br from-cyan-100/50 via-cyan-200/80 to-cyan-100/20 backdrop-blur-xl",
         )}
       >
-        {/* Glassmorphic overlay  for the background to be more soft (less sharp gradient)*/}
-        <div className="absolute inset-0 bg-white/15 backdrop-blur-xl" />
-
-        {/* Top blur fade overlay */}
-        <div
-          className={cn(
-            "pointer-events-none absolute top-0 right-0 left-0 z-50 h-24 backdrop-blur-xs",
-          )}
-          style={{
-            maskImage: "linear-gradient(to bottom, black 0%, black 60%, transparent 100%)",
-            WebkitMaskImage: "linear-gradient(to bottom, black 0%, black 60%, transparent 100%)",
-          }}
-        ></div>
-
-        {/* Bottom blur fade overlay */}
-        <div
-          className="pointer-events-none absolute right-0 bottom-0 left-0 z-50 h-8 backdrop-blur-xs"
-          style={{
-            maskImage: "linear-gradient(to top, black 0%, black 60%, transparent 100%)",
-            WebkitMaskImage: "linear-gradient(to top, black 0%, black 60%, transparent 100%)",
-          }}
-        ></div>
+        <Overlays />
 
         {/* Content */}
         <div className="relative flex items-center px-8 pt-5">
@@ -98,6 +77,41 @@ export default function WebSocketEvents({
         </div>
       </div>
     </div>
+  );
+}
+
+function Overlays() {
+  return (
+    <>
+      {/* Blurry/soft overlay on container background for a less sharp gradient */}
+      <div className={cn("absolute inset-0 bg-white/15 backdrop-blur-xl")} />
+
+      {/* Top blur fade overlay */}
+      <div
+        className={cn(
+          "pointer-events-none absolute",
+          "top-0 right-0 left-0 z-50 h-24",
+          "backdrop-blur-xs",
+        )}
+        style={{
+          maskImage: "linear-gradient(to bottom, black 0%, black 60%, transparent 100%)",
+          WebkitMaskImage: "linear-gradient(to bottom, black 0%, black 60%, transparent 100%)",
+        }}
+      ></div>
+
+      {/* Bottom blur fade overlay */}
+      <div
+        className={cn(
+          "pointer-events-none absolute",
+          "right-0 bottom-0 left-0 z-50 h-8",
+          "backdrop-blur-xs",
+        )}
+        style={{
+          maskImage: "linear-gradient(to top, black 0%, black 60%, transparent 100%)",
+          WebkitMaskImage: "linear-gradient(to top, black 0%, black 60%, transparent 100%)",
+        }}
+      ></div>
+    </>
   );
 }
 
