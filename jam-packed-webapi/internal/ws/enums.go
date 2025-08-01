43c15f0e89ba2f1126981353d2d938e019e9fd90
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-02 00:16:28 +1000

    merge job status into ws enum

--- jam-packed-webapi/internal/ws/enums.go
+++ jam-packed-webapi/internal/ws/enums.go
@@ -1,11 +1,17 @@
 package ws
 
 const (
-	EventUserJoined   = "user_joined"
-	EventUserLeft     = "user_left"
-	EventError        = "error"
-	EventPromptPin    = "prompt_pin"
-	EventIncorrectPin = "incorrect_pin"
-	EventCurrentUsers = "current_users"
+	UserJoined           string = "user_joined"
+	UserLeft             string = "user_left"
+	Error                string = "error"
+	BadPin               string = "bad_pin"
+	IncorrectPin         string = "incorrect_pin"
+	CurrentUsers         string = "current_users"
+	JobReturnedFromCache string = "job_returned_from_cache"
+	JobQueued            string = "job_queued"
+	JobStarted           string = "job_started"
+	JobRetry             string = "job_retry"
+	JobFailed            string = "job_failed"
+	JobSucceeded         string = "job_succeeded"
 	// ...Job status enum
 )
