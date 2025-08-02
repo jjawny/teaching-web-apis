875a2f8f03d8a44e5d3f2e2f90f7e912b2820daa
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-02 19:51:43 +1000

    new loading page

--- jam-packed-webapp/client/components/LoadingPage.tsx
+++ jam-packed-webapp/client/components/LoadingPage.tsx
@@ -1,10 +1,12 @@
-export function LoadingPage({ progress }: { progress: number }) {
+import { Loader2Icon } from "lucide-react";
+
+export function LoadingPage() {
   return (
     <div className="grid h-screen w-screen place-content-center">
-      <h1 className="font-instrument-serif relative max-w-[500px] text-center text-6xl font-extrabold text-stone-400 opacity-10 select-none">
-        The Most Bulletproof HTTP Request
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !opacity-100">
-          {/* TODO: progress goes here using progress prop*/}
+      <h1 className="font-instrument-serif relative max-w-[500px] text-center text-6xl font-extrabold text-stone-400 select-none">
+        <span className="opacity-10">The Most Bulletproof HTTP Request</span>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Loader2Icon className="h-20 w-20 animate-spin text-cyan-100" strokeWidth={3} />
         </div>
       </h1>
     </div>
