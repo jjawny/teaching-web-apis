c439e85e5c69a77a85c942569fbf68b83a109d84
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-03 00:13:14 +1000

    softer credits text

--- jam-packed-webapp/client/components/Credits.tsx
+++ jam-packed-webapp/client/components/Credits.tsx
@@ -2,7 +2,12 @@ import { cn } from "../utils/cn";
 
 export default function Credits({ className }: { className?: string }) {
   return (
-    <div className={cn(className, "font-instrument-serif font-bold filter-[url('#glow-outline')]")}>
+    <div
+      className={cn(
+        className,
+        "font-instrument-serif font-bold text-stone-400 filter-[url('#glow-outline')]",
+      )}
+    >
       @Jawny
     </div>
   );
