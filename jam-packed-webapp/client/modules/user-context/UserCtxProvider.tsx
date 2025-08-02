881ebf1343449a51db0e433b4084d7c545be636e
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-02 19:56:26 +1000

    new architecture for userctx

--- jam-packed-webapp/client/modules/user-context/UserCtxProvider.tsx
+++ jam-packed-webapp/client/modules/user-context/UserCtxProvider.tsx
@@ -1,36 +1,55 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 import { LoadingPage } from "~/client/components/LoadingPage";
-import { useLoadingProgress } from "./helpers";
+import { ANON_ROUTES, LOGIN_ROUTE } from "~/shared/constants";
 import { userCtx } from "./UserCtx";
 
 /**
- * - Blocks the UI (children) from rendering until the user has loaded
- * - Why? this avoids any FOUC by waiting for the user to load into the UserCtx
- * - This also abstracts the NextAuth session dependency (rest of the app uses
- *   UserCtx and we can always switch auth packages w minimal code changes)
+ * INITIALIZES the UserCtx store.
+ * Also a backup redirect gate in-case Edge middleware fails to redirect; known NextJS issues showing a flash of the pre-rendered HTML from SSR.
+ * To avoid this FOUC, before redirecting, always show a loading page.
+ *
+ * Notes for using RQ to fetch data for the UserCtx:
+ *  - If we call any React Queries here, we run the risk of a full page (children) re-render and a flash of the loading page.
+ *  - How? If another component fetches or triggers a global invalidation for the same query (queryKey).
+ *  - To avoid this, this provider must pass a unique queryKey (e.g. ["profile", "for-provider"]) to ensure no other component
+ *      can trigger re-renders for this provider.
+ *  - We also need to override any defaults like refetchOnWindowFocus to ensure the query only runs once.
  */
 export function UserCtxProvider(props: { children: ReactNode }) {
-  const { setUser, setAuthStatus } = userCtx();
-  const { data: session, status: userStatus } = useSession();
-
-  const loadingBools = [userStatus === "loading"];
-  const loadingPercentage = useLoadingProgress(loadingBools);
+  const pathname = usePathname();
+  const router = useRouter();
+  const setUser = userCtx((ctx) => ctx.setUser);
+  const setUserStatus = userCtx((ctx) => ctx.setUserStatus);
+  const { data: session, status: sessionStatus } = useSession();
+  const isOnAnonRoute = ANON_ROUTES.some((route) => pathname.startsWith(route));
 
   useEffect(
-    function sync() {
-      console.debug("Syncing UserCtx");
+    function init() {
+      console.debug("Initializing UserCtx");
       setUser(session?.user);
-      setAuthStatus(userStatus);
+      setUserStatus(sessionStatus);
     },
-    [session?.user, userStatus, setUser, setAuthStatus],
+    [session?.user, sessionStatus, setUser, setUserStatus],
   );
 
-  if (loadingPercentage !== 100) {
-    return <LoadingPage progress={loadingPercentage} />;
-  }
+  useEffect(
+    function Redirect() {
+      if (!isOnAnonRoute && sessionStatus === "unauthenticated") {
+        console.warn("User is unauthenticated, redirecting to login");
+        router.replace(LOGIN_ROUTE);
+      }
+    },
+    [sessionStatus, isOnAnonRoute, router],
+  );
 
+  const isLoading = sessionStatus === "loading";
+  const isPendingRedirect = !isOnAnonRoute && sessionStatus === "unauthenticated";
+  if (isLoading || isPendingRedirect) {
+    return <LoadingPage />;
+  }
   return <>{props.children}</>;
 }
