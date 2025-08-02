2bd9dbe17bead34fe6f870af7c155c5a4beca8ae
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-02 19:54:35 +1000

    simplify homepage

--- jam-packed-webapp/app/page.tsx
+++ jam-packed-webapp/app/page.tsx
@@ -3,30 +3,25 @@
 import ControlPanel from "~/client/components/ControlPanel";
 import Credits from "~/client/components/Credits";
 import Hero from "~/client/components/Hero";
-import { SignInButton } from "~/client/components/auth/SignInButton";
 import { SignOutButton } from "~/client/components/auth/SignOutButton";
 import { userCtx } from "~/client/modules/user-context";
 import { cn } from "~/client/utils/cn";
 
 export default function Home() {
-  const { authStatus } = userCtx();
+  const userStatus = userCtx((ctx) => ctx.userStatus);
 
   return (
     <div
       className={cn(
-        "grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20",
-        "relative", // for <Credits>, <SignOutButton>
+        "grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16",
+        "p-8 pb-20 sm:p-20",
+        "font-sans",
+        "relative",
       )}
     >
       <main className="row-start-2 flex flex-col items-center gap-[21px] sm:items-start">
         <Hero />
-        {authStatus === "authenticated" ? (
-          <ControlPanel />
-        ) : authStatus === "loading" ? (
-          <p>Loading...</p>
-        ) : (
-          <SignInButton />
-        )}
+        {userStatus === "authenticated" ? <ControlPanel /> : <p>Loading...</p>}
       </main>
       <SignOutButton className="absolute top-0 right-0 pt-4 pr-6" />
       <Credits className="absolute bottom-0 left-0 pb-4 pl-6" />
