5b3499dba1386e7c806df37c9b225215f59532f1
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-03 22:59:19 +1000

    dedicated query fn and typed data for ext api health check

--- jam-packed-webapp/client/hooks/useHealthCheckQuery.ts
+++ jam-packed-webapp/client/hooks/useHealthCheckQuery.ts
@@ -1,16 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { clientEnv } from "~/shared/modules/env";
 
-export function useHealthCheckQuery() {
+export function useExtApiHealthCheckQuery() {
   return useQuery<boolean>({
     queryKey: ["health-check"],
-    queryFn: async () => {
-      const res = await fetch(clientEnv.NEXT_PUBLIC_BACKEND_URL);
-      return res.ok;
-    },
+    queryFn: queryFn,
     staleTime: 0, // always stale; no caching
+    retry: 3,
     refetchOnMount: true,
     refetchOnWindowFocus: true,
     refetchOnReconnect: true,
   });
+}
+
+async function queryFn(): Promise<boolean> {
+  try {
+    const res = await fetch(clientEnv.NEXT_PUBLIC_BACKEND_URL, {
+      method: "GET",
+      headers: { Accept: "application/json" },
+    });
+
+    return res.ok;
+  } catch (error) {
+    console.warn("Error checking health API token:", error);
+    return false;
+  }
 }
