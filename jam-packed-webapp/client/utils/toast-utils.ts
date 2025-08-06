5b0ef49b6aa263bf537f85bc06f6f87c9e9443b3
johnny <74356539+jjjjony@users.noreply.github.com>
2025-08-06 23:15:08 +1000

    refactor: rename showError to toastError and enhance error handling

--- jam-packed-webapp/client/utils/toast-utils.ts
+++ jam-packed-webapp/client/utils/toast-utils.ts
@@ -3,8 +3,10 @@ import { toast } from "sonner";
 /**
  * Useful for surfacing RQ errors in effects.
  */
-export function showError(error?: Error | null) {
-  if (error?.message) {
+export function toastError(error?: Error | null | string) {
+  if (typeof error === "string") {
+    toast.error(error);
+  } else if (error?.message) {
     toast.error(error.message);
   }
 }
